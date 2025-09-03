f048924b1aebd9cf6db8b56dfd5db4793217fa4d
Zeher <siamsaharear010@gmail.com>
2025-09-03 10:47:44 +0600

    write read_files function

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 import cv2
+import os
+
+def read_files():
+    current_path = os.getcwd()
+    path = os.path.join(current_path, "test_images")
+    
+    all_images = []
+    
+    if os.path.exists(path=path):
+        for file in os.listdir(path=path):
+            if file.split('.')[-1]=="png" or file.split('.')[-1]=="jpg":
+                all_images.append(os.path.join(path,file))
+            else:
+                print(f"{file} not image")
+    else:
+        print("path not valid")
+    
+    return all_images
+    
+    
+    
+    
